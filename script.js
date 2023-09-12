108b012561a4d2c9f38cd02b5c9481a466be9982
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 13:06:20 -0500

    Default display hidden

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
 
+    
     if (data.weather[0].main == "Clouds"){
         weatherIcon.src = "images/clouds.png";
     } else if (data.weather[0].main == "Clear") {
