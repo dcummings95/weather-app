98f244add64804df0d13a93b41c0392b4088f684
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 13:15:19 -0500

    Edge cases, restructuring

--- script.js
+++ script.js
@@ -7,33 +7,39 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
+    //Catch if the user enters an invalid city name
+    if (response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else{
+        let data = await response.json();
 
-    //console.log(data);
-
-    //Updates the names in accordance to the search for the API
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
-
+        //console.log(data);
     
-    if (data.weather[0].main == "Clouds"){
-        weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-        weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "images/mist.png";
-    } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "images/snow.png";
+        //Updates the names in accordance to the search for the API
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+    
+        if (data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+    
+        //Update the display style to display after city name is searched
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
     }
-
-    //Update the display style to display after city name is searched
-    document.querySelector(".weather").style.display = "block";
 }
 
 //make it so when user clicks the search button we get an on click event that calls function checkWeather(city)
