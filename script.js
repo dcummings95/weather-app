e5dfec695a6f63df142507d343f293355965a250
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-12 12:55:35 -0500

    user can search city

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const apiKey = "7c857ecece9b293b4a13489e48467655";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=winona";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     let data = await response.json();
 
     console.log(data);
 
     //Updates the names in accordance to the search for the API
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°F";
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
 
 }
 
-checkWeather();
+//make it so when user clicks the search button we get an on click event that calls function checkWeather(city)
+searchBtn.addEventListener("click", ()=>{
+    //get the value of the searchbox, so grabs what user types 
+    checkWeather(searchBox.value);
+})
